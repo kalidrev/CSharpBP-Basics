a13134e09e03f80317d6b644f4bceb16a18210d9
Aaron Duryee <kalidrev@gmail.com>
2018-08-03 16:55:23 -0500

    Provide additional classes/tests & updates
    Acme.Biz:
    - Added the Product class to manage products carried in
       inventory.
    
    - Updated the Vendor class's SendWelcomeEmail method so that the company name is not immediately adjacent to the greeting ("Hello Acme" instead of "HelloAcme"). Also trimmed the subject text so that a null company name doesn't leave a space after "Hello".
    
    Acme.Common
    - Changed LoggingServices to a static class. The only member in this class is a LogAction method, so there is no reason to instantiate.
    
    - Updated the EmailService class to account for LoggingService's change to a static class.
    
    Acme.Biz.Tests
    - Added tests for the Product class's constructors
    
    Acme.CommonTests
    - Updated the LogAction_Success test to account for LoggingService's change to a static class.

---
+++ AcmeApp/Acme.Biz/Product.cs
@@ -0,0 +1,67 @@
+﻿using System;
+ 
+namespace Acme.Biz
+{
+    /// <summary>
+    /// Manages products carried in inventory.
+    /// </summary>
+    public class Product
+    {
+        #region Constructors
+        
+        public Product()
+        {
+            Console.WriteLine("Product instance created");
+        }
+
+        public Product(int productId, string productName, string description) : this()
+        {
+            this.ProductId = productId;
+            this.ProductName = productName;
+            this.Description = description;
+
+            Console.WriteLine("Product instance has a name: " + ProductName);
+        }
+
+        #endregion
+
+        #region Properties
+
+        
+        private string productName;
+
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        private int productId;
+
+        public int ProductId
+        {
+            get { return productId; }
+            set { productId = value; }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public string SayHello()
+        {
+            return "Hello " + ProductName + " (" + ProductId + "): " + Description;
+        }
+
+        #endregion
+
+    }
+}
