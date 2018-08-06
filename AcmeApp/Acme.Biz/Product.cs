cf29a9f540b63c37c5a8debdbf14746e7e82d639
Aaron Duryee <kalidrev@gmail.com>
2018-08-06 08:26:48 -0500

    Yet more updates to Product and Product Tests
    Acme.Biz:
    
    - Updated the AssemblyInfo file so that the Acme.BizTests component has access to the internals of Property fields. This is needed to test its internal fields/properties.
    
    - Updated the ProductName property's getter so that it returns a trimmed string, and updated its setter to validate the minimum and maximum allowed length for the string before setting it.
    
    - Added the Category and SequenceNumber properties to demonstrate auto-implemented properties.
    
    - Added the ProductCode property to demonstrate expression-bodied-properties.

--- AcmeApp/Acme.Biz/Product.cs
+++ AcmeApp/Acme.Biz/Product.cs
@@ -19,6 +19,7 @@ namespace Acme.Biz
             Console.WriteLine("Product instance created");
             //this.ProductVendor = new Vendor();
             this.MinimumPrice = .96m;
+            this.Category = "Tools";
         }
 
         public Product(int productId, string productName, string description) : this()
@@ -43,8 +44,26 @@ namespace Acme.Biz
 
         public string ProductName
         {
-            get { return productName; }
-            set { productName = value; }
+            get
+            {
+                var formattedValue = productName?.Trim();
+                return formattedValue;
+            }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    ValidationMessage = "Product Name must be at least 3 characters";
+                }
+                else if (value.Length > 20)
+                {
+                    ValidationMessage = "Product Name cannot be more than 20 characters";
+                }
+                else
+                {
+                    productName = value;
+                }
+            }
         }
 
         private string description;
@@ -79,6 +98,8 @@ namespace Acme.Biz
             set { productVendor = value; }
         }
 
+        public string ProductCode => this.Category + "-" + this.SequenceNumber;
+
         private DateTime? availabilityDate;
 
         public DateTime? AvailabilityDate
@@ -86,6 +107,12 @@ namespace Acme.Biz
             get { return availabilityDate; }
             set { availabilityDate = value; }
         }
+
+        internal string Category { get; set; }
+        public int SequenceNumber { get; set; } = 1;
+
+
+        public string ValidationMessage { get; private set; }
 
 
 
@@ -102,8 +129,8 @@ namespace Acme.Biz
             var confirmation = emailService.SendMessage("New Product", this.ProductName, "sales@abc.com");
             var result = LogAction("saying Hello");
 
-            return "Hello " + ProductName + 
-                " (" + ProductId + "): " + 
+            return "Hello " + ProductName +
+                " (" + ProductId + "): " +
                 Description +
                 " Available on: " +
                 AvailabilityDate?.ToShortDateString();
